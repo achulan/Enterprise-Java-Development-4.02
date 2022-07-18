06699e2096d07d9cfd4400b63a226f9fc39c14a1
Anastasia Pr. Ch <chulymzn94@gmail.com>
2022-07-18 19:01:32 +0200

    first commit

---
+++ src/test/java/com/ironhack/EnterpriseJavaDevelopment42/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.EnterpriseJavaDevelopment42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
